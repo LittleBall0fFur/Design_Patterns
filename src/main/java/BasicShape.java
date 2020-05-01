07b4b893e2ecb11050c95959b180d601a80b339f
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2020-05-01 14:29:19 +0200

    Initial skeleton upload

---
+++ src/main/java/BasicShape.java
@@ -0,0 +1,18 @@
+import java.awt.geom.Point2D;
+
+public class BasicShape {
+    private int width= 0, height = 0;
+    Point2D position;
+
+    void Select(){
+
+    }
+
+    void Scale(){
+
+    }
+
+    void Move(){
+
+    }
+}
---
+++ src/main/java/Canvas.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class Canvas {
+    private int width= 0, height = 0;
+    private List<?> actions;
+
+    Canvas(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+}
