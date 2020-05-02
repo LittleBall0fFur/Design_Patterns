6ca97a918f8ae42acbe2b7adea5b2a7f207b828e
shcalbers <34623185+shcalbers@users.noreply.github.com>
2020-05-02 14:44:52 +0200

    Add Main class 'App.java'

---
+++ src/main/java/com/nhlstenden/designpatterns/App.java
@@ -0,0 +1,17 @@
+package com.nhlstenden.designpatterns;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO: Implement.
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+}
