6f76bb0c9da56c12e695df7064c31e592cab2b5d
Naomi Poort <naomi.roberta.ans.poort@gmail.com>
2020-05-02 16:36:08 +0200

    Application start scene added

--- src/main/java/com/nhlstenden/designpatterns/App.java
+++ src/main/java/com/nhlstenden/designpatterns/App.java
@@ -1,6 +1,11 @@
 package com.nhlstenden.designpatterns;
 
+import com.nhlstenden.designpatterns.graphics.Canvas;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -8,6 +13,12 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         // TODO: Implement.
+        Group root = new Group();
+        Scene s = new Scene(root, 1535, 800, Color.BLACK);
+        Canvas canvas = new Canvas(1485, 750, root);
+        stage.setScene(s);
+        stage.setMaximized(true);
+        stage.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/nhlstenden/designpatterns/graphics/Canvas.java
+++ src/main/java/com/nhlstenden/designpatterns/graphics/Canvas.java
@@ -1,6 +1,10 @@
 package com.nhlstenden.designpatterns.graphics;
 
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.util.List;
 
@@ -8,18 +12,17 @@ public class Canvas {
 
     private int width= 0, height = 0;
     private Scene scene;
-    private List<Object> children;
+    private javafx.scene.canvas.Canvas canvas;
+    private GraphicsContext gc;
 
-    public Canvas(int width, int height){
+    public Canvas(int width, int height, Group root){
+        canvas = new javafx.scene.canvas.Canvas(width, height);
 
+        gc = canvas.getGraphicsContext2D();
+
+        gc.setFill(Color.WHITE);
+        gc.fillRect(50, 50, width, height);
+
+        root.getChildren().add(canvas);
     }
-
-    public void addChild(BasicShape entity) {
-        children.add(entity);
-    }
-
-    public void removeChild(BasicShape entity) {
-        children.remove(entity);
-    }
-
 }
